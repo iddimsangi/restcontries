610525c0ec8087dfc14dd1122a4cad9f370839bb
iddimsangi <adammsangi@gmail.com>
2021-12-30 10:11:12 +0300

    main body structure

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import "./App.scss";
-import moon from "./img/half-moon-svgrepo-com.svg"
+import moon from "./img/half-moon-svgrepo-com.svg";
 function App() {
   return (
     <div className="App">
       <nav className="App-header">
         <h1>where in the world?</h1>
         <div>
-        <img src={moon} alt="moon" />
-        Dark Mode
+          <img src={moon} alt="moon" />
+          <span> Dark Mode</span>
         </div>
       </nav>
+      <main className="App-body"></main>
     </div>
   );
 }
