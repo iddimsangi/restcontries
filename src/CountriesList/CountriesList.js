6480f80b68b25cd25901d794c466622788c23097
iddimsangi <adammsangi@gmail.com>
2022-01-03 12:32:32 +0300

    margin setup

--- src/CountriesList/CountriesList.js
+++ src/CountriesList/CountriesList.js
@@ -5,19 +5,19 @@ import search from "../img/icons8-search.svg";
 function CountriesList() {
   return (
     <div className="countryListApp">
-      <div>
+      <div className="countryListApp--container">
         <form action="#" method="GET or POST">
-         <div>
-         <label for="fname">
-            <img src={search} alt="search" />
-          </label>
-          <input
-            type="text"
-            name="FirstName"
-            id="fname"
-            placeholder="Search for a country"
-          />
-         </div>
+          <div>
+            <label htmlFor="fname">
+              <img src={search} alt="search" />
+            </label>
+            <input
+              type="text"
+              name="FirstName"
+              id="fname"
+              placeholder="Search for a country"
+            />
+          </div>
           <select name="countries" id="countries">
             <option value="volvo">Volvo</option>
             <option value="saab">Saab</option>
@@ -25,16 +25,18 @@ function CountriesList() {
             <option value="audi">Audi</option>
           </select>
         </form>
+
+        <div className="countryList">
+          <Country />
+          <Country />
+          <Country />
+          <Country />
+          <Country />
+          <Country />
+          <Country />
+          <Country />
+        </div>
       </div>
-      <div className="countryList">
-      <Country />
-      <Country />
-      <Country />
-      <Country />
-     
-      </div>
-   
-    
     </div>
   );
 }
