da4eca833e10e0e952c778698532e94db0bb954f
iddimsangi <adammsangi@gmail.com>
2022-01-05 16:27:30 +0300

    white mode theme on app component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
+import { useState } from "react";
 import "./App.scss";
-import CountriesList from "./CountriesList/CountriesList"
-import moon from "./img/icon-moon.svg";
-function App() {
+import CountriesList from "./CountriesList/CountriesList";
+import { ReactComponent as Moon } from "./img/half-moon-shape-svgrepo-com.svg";
+import { ReactComponent as MoonIcon } from "./img/icon-moon.svg";
+// import moonIcon from "./img/icon-moon.svg";
+function App(props) {
+  const [theme, settheme] = useState(true);
   return (
-    <div className="App">
-      <nav className="App-header">
-        <h1>where in the world?</h1>
-        <div>
-          <img src={moon} alt="moon" />
-          <span> Dark Mode</span>
-        </div>
-      </nav>
-      <main className="App-body">
-        <CountriesList/>
-      </main>
+    <div>
+      <input type="checkbox" id="nav_toggle" className="inputChek" />
+      <div className="App">
+        <nav className="App-header">
+          <h1>where in the world?</h1>
+          <div>
+            <label htmlFor="nav_toggle">
+              <Moon
+                onClick={() => settheme(!theme)}
+                className={theme ? "logoSvg2" : "logoSvg"}
+              />
+            </label>
+
+            {/* <MoonIcon className={theme ? "logoSvg" : "logoSvg2"}/> */}
+            {/* <img src={moon} alt="moon" /> */}
+            {/* <img src={moonIcon} alt="cc"/> */}
+            <span className={theme ? "navText" : "navText2"}> Dark Mode</span>
+          </div>
+        </nav>
+        <main className="App-body">
+          <CountriesList />
+        </main>
+      </div>
     </div>
   );
 }
